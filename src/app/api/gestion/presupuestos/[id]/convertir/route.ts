b804cad88fa846aa858ac5a4923158046f50b260
leonardo <leonardofarena@gmail.com>
2025-12-10 11:39:17 -0300

    pi

--- src/app/api/gestion/presupuestos/[id]/convertir/route.ts
+++ src/app/api/gestion/presupuestos/[id]/convertir/route.ts
@@ -4,11 +4,13 @@ import connectDB from '@/app/lib/mongoose';
 import Pedido from '@/app/models/Pedido';
 import Presupuesto from '@/app/models/Presupuesto';
 
-export async function POST(request: NextRequest)  {
-  
+export async function POST(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
   try {
     await connectDB();
-    const { params } = await request.json();
+
     const { id } = params;
 
     const presupuesto = await Presupuesto.findById(id);
