9f970f5e8127361058954d170777282f38345360
leonardo <leonardofarena@gmail.com>
2025-12-30 14:02:02 -0300

    mkkkat

--- src/app/gestion/clientes/page.tsx
+++ src/app/gestion/clientes/page.tsx
@@ -283,7 +283,7 @@ export default function ClientesPage() {
     }
   };
   // -------------------------------
-  // 🚨 Render condicional de autorización
+  // 🚨 Render condicional de autorización nnn
   // -------------------------------
   if (!isAuthorized) return <div className="p-6 text-center text-gray-400">Validando acceso...</div>;
 
