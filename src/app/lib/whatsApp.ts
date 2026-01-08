2b024ec3c15cb2c6aad9e6098e3eaae289146b2b
leonardo <leonardofarena@gmail.com>
2026-01-08 15:46:53 -0300

    jhhu

--- src/app/lib/whatsApp.ts
+++ src/app/lib/whatsApp.ts
@@ -1,6 +1,6 @@
 import { formatARS } from './formatcurrenci';
 
-export function sendWhatsApp(cart: any[], userName?: string, userEmail?: string) {
+export function sendWhatsApp(cart: any[]) {
   if (!cart.length) return;
 
   let total = 0;
@@ -20,14 +20,7 @@ export function sendWhatsApp(cart: any[], userName?: string, userEmail?: string)
   message += `PRODUCTOS:\n${productList.trim()}\n\n`;
   message += `TOTAL: ${formatARS(total)}\n\n`;
 
-  // Si el usuario está logueado, incluimos sus datos
-  if (userName && userEmail) {
-    message += `CLIENTE:\nNombre: ${userName}\nEmail: ${userEmail}\n\n`;
-  } else {
-    // Si NO está logueado, mensaje amable de recordatorio
-    message += `ℹ️ *¿Sabías que podrías agilizar tu próxima compra?*\nRegístrate en nuestro sitio con tu cuenta de Gmail o tus datos para guardar tu historial y disfrutar de una experiencia más rápida y ofertas exclusivas.\n\n`;
-  }
-
+ 
   message += `Origen: www.elvaquianodigital.com.ar\n¿Listo para confirmar este pedido?`;
 
   const phone = '5492224492051';
