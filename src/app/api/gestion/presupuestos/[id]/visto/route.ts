76693ce093a65f19b3ca9c8e84b48178deab390c
leonardo <leonardofarena@gmail.com>
2026-01-30 19:06:22 -0300

    bbb

--- src/app/api/gestion/presupuestos/[id]/visto/route.ts
+++ src/app/api/gestion/presupuestos/[id]/visto/route.ts
@@ -1,15 +1,17 @@
+import { NextRequest, NextResponse } from 'next/server';
 import connectDB from '@/app/lib/mongoose';
 import Presupuesto from '@/app/models/Presupuesto';
-import { NextResponse } from 'next/server';
 
 export async function PATCH(
-  _: Request,
-  { params }: { params: { id: string } }
+  _req: NextRequest,
+  context: { params: { id: string } }
 ) {
   await connectDB();
 
-  await Presupuesto.findByIdAndUpdate(params.id, {
-    vistoPorAdmin: true
+  const { id } = context.params;
+
+  await Presupuesto.findByIdAndUpdate(id, {
+    vistoPorAdmin: true,
   });
 
   return NextResponse.json({ ok: true });
